527e0ab4fed3d81b93c994bb5b83406c519174bd
AliAkbari2 <72106652+AliAkbari2@users.noreply.github.com>
2020-12-24 01:40:03 -0700

    Add files via upload

---
+++ Assignments/Assignment #2/SLLBubbleSort.java
@@ -0,0 +1,22 @@
+package hw2.student;
+
+public class SLLBubbleSort implements BubbleSortInterface { 
+
+  /**
+   * A method that will sort a singly linked list that holds Comparable 
+   * elements. Bubble sort is used to sort the elements. In this version of 
+   * bubble sort, the largest element will bubble to the right in each pass.  
+   * Note that the linked list is sorted in place, i.e. the specified list is
+   * modified; a copy is not made. 
+   * 
+   * @param list The SLL list to sort
+   */
+  
+  public <T extends Comparable<? super T>> void BubbleSort( SLL<T> list ) {
+    
+    // TO DO 
+    
+    return;
+  }
+  
+}
